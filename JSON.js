0cdc10b41a1e9e6142a58dd853961f3c5e9de5de
Serge Fonville <serge.fonville@gmail.com>
2017-04-21 12:29:09 +0200

    Initial Commit

---
+++ JSON.js
@@ -0,0 +1,49 @@
+var JSON = {};
+JSON['stringifyArray'] = function(array) {
+    var jsonData = '[';
+    for(var i = 0; i < array.length; i++) {
+        if(array[i] instanceof Array)
+            jsonData += JSON['stringifyArray'](array[i])
+        else if(array[i] instanceof Object)
+            jsonData += JSON['stringifyObject'](array[i]);
+        else if(typeof array[i] == 'string')
+            jsonData += '"' + array[i] + '"';
+        else 
+            jsonData += array[i];
+        jsonData += ',';
+    }
+    jsonData = jsonData.replace(/,$/g, '');
+    jsonData += ']';
+    return jsonData;
+};
+JSON['stringifyObject'] = function(object) {
+    var jsonData = '{';
+    for(var property in object) {
+        jsonData += '"' + property +'":';
+        if(object[property] instanceof Array)
+            jsonData += JSON['stringifyArray'](object[property])
+        else if(object[property] instanceof Object)
+            jsonData += JSON['stringifyObject'](object[property])
+        else if(typeof object[property] == 'string')
+            jsonData += '"' + object[property] + '"';
+        else 
+            jsonData += object[property];
+        jsonData += ',';
+    }
+    jsonData = jsonData.replace(/,$/g, '');
+    jsonData += '}';
+    return jsonData;
+};
+JSON["parse"] = function(string) {
+	return (new Function ('return ' + string))()
+};
+
+JSON['stringify'] = function(object) {    
+    if(object instanceof Array)
+        return JSON['stringifyArray'](object)
+    if(object instanceof Object)
+        return JSON['stringifyObject'](object);
+    if(typeof object == 'string')
+        return '"' + object + '"';
+    return object;    
+};
---
+++ README.md
@@ -0,0 +1,2 @@
+# JavaScript
+Just a bunch of JavaScript files created in my endeavour to learn JavaScript.
