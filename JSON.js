98403b21203b4d136d422f5cc319d166b77ec83e
Serge Fonville <serge.fonville@gmail.com>
2017-04-21 18:48:04 +0200

    Replaced tabs with spaces

--- JSON.js
+++ JSON.js
@@ -1,23 +1,23 @@
 var JSON = {
-	parse: function(string) {
-		return (new Function ('return ' + string))()
-	},
-	stringify: function(object) {
-		if(object instanceof Array) {
-			var jsonArrayArray = [];
-			for(var i = 0; i < object.length; i++)
-				jsonArrayArray.push(this.stringify(object[i]));
-			return '[' + jsonArrayArray.join(',') + ']'; 
-		}
-		if(object instanceof Object) {
-			var jsonObjectArray = [];
-		    for(var property in object)
-				jsonObjectArray.push('"' + property + '":' + this.stringify(object[property]));
-			return '{' + jsonObjectArray.join(',') + '}';
-		}
-		if(typeof object === 'string')
-			return '"' + object + '"';
-		else
-			return object;
-	}
+    parse: function(string) {
+        return (new Function ('return ' + string))()
+    },
+    stringify: function(object) {
+        if(object instanceof Array) {
+            var jsonArrayArray = [];
+            for(var i = 0; i < object.length; i++)
+                jsonArrayArray.push(this.stringify(object[i]));
+            return '[' + jsonArrayArray.join(',') + ']'; 
+        }
+        if(object instanceof Object) {
+            var jsonObjectArray = [];
+            for(var property in object)
+                jsonObjectArray.push('"' + property + '":' + this.stringify(object[property]));
+            return '{' + jsonObjectArray.join(',') + '}';
+        }
+        if(typeof object === 'string')
+            return '"' + object + '"';
+        else
+            return object;
+    }
 }
